0b7be2beaedbdefe3dda46068ec5482fbff475b2
Edip YILDIZ <edip.yildiz@hotmail.com>
2017-09-18 00:18:18 +0300

    get user from ahenk

---
+++ ahenk-usb-ltsp/usb-ltsp/get_users.py
@@ -0,0 +1,56 @@
+import json
+import os
+from base.plugin.abstract_plugin import AbstractPlugin
+
+class GetUser(AbstractPlugin):
+    def __init__(self, task, context):
+        super(AbstractPlugin, self).__init__()
+        self.task = task
+        self.context = context
+        self.logger = self.get_logger()
+        self.message_code = self.get_message_code()
+
+    def handle_task(self):
+        try:
+            users=self.get_home_users()
+            self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                             message='Login olan kullanıcılar başarı ile getirildi.',
+                                             data=json.dumps(
+                                                 {
+                                                     'Result': 'İşlem Başarı ile gercekleştirildi',
+                                                     'users': users
+                                                 }
+                                             ),
+                                             content_type=self.get_content_type().APPLICATION_JSON.value)
+        except Exception as e:
+            self.logger.error('A problem occurred while editing usb privilege. Error Message: {0}'.format(str(e)))
+            self.context.create_response(code=self.message_code.TASK_ERROR.value,
+                                         message='Kullanıcılar getirilirken düzenlenirken hata oluştu',
+                                         content_type=self.get_content_type().APPLICATION_JSON.value)
+
+
+        self.logger.debug('Restarted cron service')
+
+    def get_home_users(self):
+        users = []
+        directories = os.listdir('/home')
+        for directory in directories:
+            if os.path.isdir('/home/' + directory):
+                result = os.popen("grep " + directory + " /etc/passwd").read()
+                if result:
+                    user = dict()
+                    user['username'] = str(directory)
+                    result_code, p_out, p_err = self.execute("groups " + str(directory))
+
+                    if 'fuse' in p_out:
+                        user["statusCode"] = 1
+                    else :
+                        user["statusCode"] = 0
+
+                    users.append(user)
+        return users
+
+
+def handle_task(task, context):
+    classs = GetUser(task, context)
+    classs.handle_task()
