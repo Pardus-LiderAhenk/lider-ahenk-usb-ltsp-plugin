b5576bd031b835919ff166bcf030d2ce6d5427cf
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-11-21 14:19:09 +0300

    bugfix

--- ahenk-usb-ltsp/usb-ltsp/get_users.py
+++ ahenk-usb-ltsp/usb-ltsp/get_users.py
@@ -12,24 +12,57 @@ class GetUser(AbstractPlugin):
 
     def handle_task(self):
         try:
-            users=self.get_home_users()
-            self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
-                                             message='Login olan kullanıcılar başarı ile getirildi.',
-                                             data=json.dumps(
-                                                 {
-                                                     'Result': 'İşlem Başarı ile gercekleştirildi',
-                                                     'users': users
-                                                 }
-                                             ),
-                                             content_type=self.get_content_type().APPLICATION_JSON.value)
+            # for single selected user
+            selectedUser= None
+
+            if 'selectedUser' in self.task :
+                selectedUser = str(self.task['selectedUser'])
+                self.logger.debug('Selected User : '+ str(selectedUser))
+
+                if selectedUser :
+
+                    users = []
+
+                    if os.path.isdir('/home/' + str(selectedUser)):
+                        result = os.popen("getent passwd | grep " + selectedUser).read()
+                        if result:
+                            user = dict()
+                            user['username'] = str(selectedUser)
+                            result_code, p_out, p_err = self.execute("groups " + str(selectedUser))
+
+                            if 'fuse' in p_out:
+                                user["statusCode"] = 1
+                            else:
+                                user["statusCode"] = 0
+
+                            users.append(user)
+
+                    self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                                 message='Login olan kullanıcılar başarı ile getirildi.',
+                                                 data=json.dumps(
+                                                     {
+                                                         'Result': 'İşlem Başarı ile gercekleştirildi',
+                                                         'users': users
+                                                     }
+                                                 ),
+                                                 content_type=self.get_content_type().APPLICATION_JSON.value)
+
+            else:
+                users=self.get_home_users()
+                self.context.create_response(code=self.message_code.TASK_PROCESSED.value,
+                                                 message='Login olan kullanıcılar başarı ile getirildi.',
+                                                 data=json.dumps(
+                                                     {
+                                                         'Result': 'İşlem Başarı ile gercekleştirildi',
+                                                         'users': users
+                                                     }
+                                                 ),
+                                                 content_type=self.get_content_type().APPLICATION_JSON.value)
         except Exception as e:
             self.logger.error('A problem occurred while editing usb privilege. Error Message: {0}'.format(str(e)))
             self.context.create_response(code=self.message_code.TASK_ERROR.value,
                                          message='Kullanıcılar listelenirken hata oluştu',
                                          content_type=self.get_content_type().APPLICATION_JSON.value)
-
-
-        self.logger.debug('Restarted cron service')
 
     def get_home_users(self):
         users = []
